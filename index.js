2ee72f6205f01bb1f47508ee6827df7487b9b1dc
sl <tyu.sam@gmail.com>
2017-06-26 21:47:35 -0400

    Added the logic for the bot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Botkit = require('botkit');
+var kv = require('beepboop-persist')()
 
 const token = process.env.SLACK_TOKEN;
 
@@ -29,25 +30,57 @@ if (token) {
 }
 
 // when the bot joins a channel, give information about its usage
-controller.on('bot_channel_join', (bot message) => {
-  bot.reply(message, 'Hi! I\'m Link Bot! To let me know what github repository to link to, type: @linkbot <github-repo>');
+controller.on('bot_channel_join', (bot, message) => {
+  bot.reply(message, 'Hi! I\'m Link Bot! To let me know what github repository to link to, type: `@linkbot set-repo <github-repo>`');
 });
 
-controller.on('direct_mention',function(bot,message) {
-  console.log(message);
+controller.on('direct_mention',function(bot, message) {
+  if (message.text.startsWith('set-repo ') && message.text.length > 9) {
+    var name = message.text.substring(9)
+    kv.set(message.team, name, (err) => {
+      if (!err) {
+        bot.reply(message, 'Successfully set the repository to ' + name);
+      } else {
+        bot.reply(message, 'Error setting the repository:\n' + err);
+      }
+    });
+  } else if (message.text.startsWith('get-repo')) {
+    kv.get(message.team, (err, val) => {
+      if (err) {
+        bot.reply(message, 'Error getting the slack team\'s github repository: ' + err);
+      }
+      if (val) {
+        bot.reply(message, 'The repository is currently set to: ' + val);
+      } else {
+        bot.reply(message, 'There is currently no configured repository for this team! Use `@linkbot set-repo <github-repo>` to specify one!');
+      }
+    });
+
+  }
 });
 
-controller.hears(['\#[0-9]+'], ['message_received'], (bot, message) => {
+controller.hears('#[0-9]+', ['ambient'], (bot, message) => {
+  console.log('heard message');
   const re = /\#([0-9]+)/g;
   var links = [];
   var m;
   do {
-    m = re.exec(message);
+    m = re.exec(message.text);
     if (m) {
-      links.push(m);
+      links.push(m[1]);
     }
   } while (m);
-  bot.reply(message, links.join(', '));
+  if (links.length !== 0) {
+    // get the repository name
+    kv.get(message.team, (err, val) => {
+      if (err) {
+        bot.reply(message, "I don't know what github repository to look for issues in! Be sure to tell me your repo using `@linkbot set-repo <github-repo>`");
+      }
+      bot.reply(message, links.map((x) => {
+        return '<https://github.com/' + val + '/issues/' + x + '|' + val + ': #' + x + '>';
+      }).join(', '));
+    });
+  }
 });
 
 
