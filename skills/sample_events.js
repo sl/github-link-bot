5b17bfa65120a6da0d84794cc0590b0516dbb5d0
sl <tyu.sam@gmail.com>
2017-06-26 22:53:53 -0400

    Updated project structure

---
+++ skills/sample_events.js
@@ -0,0 +1,6 @@
+
+module.exports = controller => {
+  controller.on('user_channel_join,user_group_join', (bot, message) => {
+    bot.reply(message, 'Welcome, <@' + message.user + '>')
+  })
+}
---
+++ skills/sample_middleware.js
@@ -0,0 +1,14 @@
+
+module.exports = controller => {
+  // controller.middleware.receive.use((bot, message, next) => {
+  //   // do something...
+  //   console.log('RCVD:', message)
+  //   next()
+  // })
+
+  // controller.middleware.send.use((bot, message, next) => {
+  //   // do something...
+  //   console.log('SEND:', message)
+  //   next()
+  // })
+}
